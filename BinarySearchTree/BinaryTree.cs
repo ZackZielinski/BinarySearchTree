02b3deda275c7acac127a60420c571007abdede9
ZackZielinski <Zackzielinski0@gmail.com>
2017-08-22 13:32:11 -0500

    Inital commit

---
+++ BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinaryTree
+    {
+        private Node Root;
+
+        public BinaryTree()
+        {
+            Root = null;
+        }
+
+        public void CraftTree()
+        {
+            Root = new Node('Z');
+        }
+
+    }
+}
---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        public int Data;
+        public Node LeftChild;
+        public Node RightChild;
+
+        public Node(int Data)
+        {
+            this.Data = Data;
+            LeftChild = null;
+            RightChild = null;
+        }
+    }
+}
